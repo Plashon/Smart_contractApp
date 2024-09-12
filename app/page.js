d3f0ca7a629313724d1abb28e26d13e8f5329d8a
Plashon <fourza12poobate@gmail.com>
2024-09-12 12:13:52 +0700

    first commit

--- app/page.js
+++ app/page.js
@@ -1,95 +1,163 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import React, { useState, useEffect } from "react";
 
-export default function Home() {
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  Chip,
+  Stack,
+  Container,
+  Card,
+  CardContent,
+  TextField,
+  Box,
+  IconButton,
+  Divider,
+} from "@mui/material";
+
+import abi from "./abi.json";
+import MenuIcon from "@mui/icons-material/Menu";
+
+import { ethers } from "ethers";
+import { formatEther, parseUnits } from "@ethersproject/units";
+
+import { initializeConnector } from "@web3-react/core";
+import { MetaMask } from "@web3-react/metamask";
+
+const [metaMask, hooks] = initializeConnector(
+  actions => new MetaMask({ actions })
+);
+const { useChainId, useAccounts, useIsActivating, useIsActive, useProvider } =
+  hooks;
+const contractChain = 11155111;
+const contractAddress = "0xc417c3eb7bba3E0902951422E272949838f82D01"; //address of smart contract
+const getAddressTxt = (str, s = 6, e = 6) => {
+  if (str) {
+    return `${str.slice(0, s)}...${str.slice(str.length - e)}`;
+  }
+  return "";
+};
+
+export default function Page() {
+  const chainId = useChainId();
+  const accounts = useAccounts();
+  const isActive = useIsActive();
+
+  const provider = useProvider();
+  const [error, setError] = useState(undefined);
+
+  const [balance, setBalance] = useState("");
+  useEffect(() => {
+    const fetchBalance = async () => {
+      const signer = provider.getSigner();
+      const smartContract = new ethers.Contract(contractAddress, abi, signer);
+      const myBalance = await smartContract.balanceOf(accounts[0]);
+      console.log(formatEther(myBalance));
+      setBalance(formatEther(myBalance));
+    };
+    if (isActive) {
+      fetchBalance();
+    }
+  }, [isActive]);
+
+  const [ETHValue, setETHValue] = useState(0);
+  const handleBuy = async () => {
+    if (ETHValue <= 0) {
+      return;
+    }
+
+    const signer = provider.getSigner();
+    const smartContract = new ethers.Contract(contractAddress, abi, signer);
+    const weiValue = parseUnits(ETHValue.toString(), "ether");
+    const tx = await smartContract.buy({
+      value: weiValue.toString(),
+    });
+    console.log("Transaction hash:", tx.hash);
+  };
+
+  useEffect(() => {
+    void metaMask.connectEagerly().catch(() => {
+      console.debug("Failed to connect eagerly to metamask");
+    });
+  }, []);
+
+  const handleConnect = () => {
+    metaMask.activate(contractChain);
+  };
+
+  const handleDisconnect = () => {
+    metaMask.resetState();
+    alert(
+      "To fully disconnect, please remove this site from MetaMask's connected sites by locking metamask."
+    );
+  };
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Plashon App CryptoExchange
+            </Typography>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+            {!isActive ? (
+              <Button variant="contained" onClick={handleConnect}>
+                Connect Wallet
+              </Button>
+            ) : (
+              <Stack direction="row" spacing={1}>
+                <Chip label={getAddressTxt(accounts[0])} variant="outlined" />
+
+                <Button
+                  variant="contained"
+                  color="inherit"
+                  onClick={handleDisconnect}
+                >
+                  Disconnect
+                </Button>
+              </Stack>
+            )}
+          </Toolbar>
+        </AppBar>
+      </Box>
+
+      <Container maxWidth="sm" sx={{ mt: 2 }}>
+        {isActive ? (
+          <>
+            <Card>
+              <CardContent>
+                <Stack spacing={2}>
+                  <Typography>PLS</Typography>
+                  <TextField label="Address" value={accounts[0]} />
+                  <TextField label="PLS Balance" value={balance} />
+                  <Divider />
+                  <Typography>Buy PLS (1 ETH = 10 PLS)</Typography>
+                  <TextField
+                    label="ETH"
+                    type="number"
+                    onChange={e => setETHValue(e.target.value)}
+                  />
+                  <Button variant="contained" onClick={handleBuy}>
+                    Buy
+                  </Button>
+                </Stack>
+              </CardContent>
+            </Card>
+          </>
+        ) : null}
+      </Container>
     </div>
   );
 }
